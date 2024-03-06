08a29e61c317c2f88f96d999d2b87805f4f034b9
D3Kam <154370264+D3Kam@users.noreply.github.com>
2024-03-06 22:28:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 function collapseNavbar() {
-    // Get the navbar toggle button
     const navbarToggleButton = document.querySelector('.navbar-toggler');
 
-    // Check if the navbar toggle button is visible and the navbar is currently expanded
     if (navbarToggleButton && !navbarToggleButton.classList.contains('collapsed')) {
-        // Simulate a click event on the navbar toggle button to collapse the navbar
         navbarToggleButton.click();
     }
 }
 
-// Listen for the scroll event on the window
+
 window.addEventListener('scroll', function() {
-    // Call the collapseNavbar function when the user scrolls
+    
     collapseNavbar();
+});
+window.addEventListener('click', function() {
+    
+    collapseNavbar();
+});
+
+const idLinks = document.querySelectorAll('a[href^="#"]');
+idLinks.forEach(link => {
+
+    link.addEventListener('click', function() {
+        collapseNavbar();
+    });
 });
